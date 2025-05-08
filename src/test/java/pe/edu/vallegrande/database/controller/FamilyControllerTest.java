09ef488802d5348ba0af7955e735dee8c5d771fb
Diego Ramos Sanchez <127451062+AlexanderRamosSanchez@users.noreply.github.com>
2025-05-08 11:17:24 -0500

    [MEDIUM] : microservice

--- src/test/java/pe/edu/vallegrande/database/controller/FamilyControllerTest.java
+++ src/test/java/pe/edu/vallegrande/database/controller/FamilyControllerTest.java
@@ -15,7 +15,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest
 class FamilyControllerTest { // Modificador 'public' eliminado
 
     private FamilyController familyController;
